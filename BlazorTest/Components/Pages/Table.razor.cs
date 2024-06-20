de04eaaef7cfeb24ef47d8eddad9eaf18172c95c
aleksanderphan <aleksanderphan11@gmail.com>
2024-06-20 09:46:10 +0700

    minor adjustment
    minor view/styles adjustment

--- BlazorTest/Components/Pages/Table.razor.cs
+++ BlazorTest/Components/Pages/Table.razor.cs
@@ -72,12 +72,5 @@ namespace BlazorTest.Components.Pages
         {
             return string.IsNullOrWhiteSpace(product.Name) || string.IsNullOrWhiteSpace(product.Color) || product.Price <= 0;
         }
-
-        private string GetSaveButtonClass()
-        {
-            return IsSaveDisabled()
-                ? "bg-gray-500 text-white font-bold py-2 px-4 rounded cursor-not-allowed"
-                : "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded";
-        }
     }
 }
